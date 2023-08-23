e70ff8f8eb8d165d17384a288c192de7a35b7136
Aleksandr Gegeshidze <gegcan@yandex.ru>
2023-08-23 22:44:39 +0500

    Initial Commit

---
+++ Сounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Сounter
+//
+//  Created by Александр Гегешидзе on 23.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
